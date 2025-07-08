a63277ed1830bce4e48fd2c8eff067b7b7a3d243
chinnayyayash <chinnayya.chintha@yash.com>
2025-07-08 15:08:35 +0530

    added condition for region check

--- variables.tf
+++ variables.tf
@@ -2,4 +2,10 @@ variable "aws_region" {
   description = "The Home Region for Control Tower, must be us-east-1"
   type        = string
   default     = "us-east-1"
+
+  validation {
+    condition     = var.aws_region == "us-east-1"
+    error_message = "Region must be home region of control tower, us-east-1"
+  }
+
 }
